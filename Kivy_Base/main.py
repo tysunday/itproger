eacce79b1664dfec72ebcbdf9d119c4cd9dc7e8b
tysunday <120739860+tysunday@users.noreply.github.com>
2023-10-19 16:54:00 +0500

    Add files via upload

--- Kivy_Base/main.py
+++ Kivy_Base/main.py
@@ -1,14 +1,27 @@
 from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ObjectProperty
+from kivy.uix.gridlayout import GridLayout
+from kivy.config import Config
+from kivy.core.window import Window
 
-class Container(BoxLayout):
-    text_input = ObjectProperty
-    label_text = ObjectProperty
+Config.set('kivy', 'keyboard mode', 'systemanddock')
 
-    def change_something(self):
-        self.label_text.text = self.text_input.text
+Window.size = (360, 640)
+
+class Container(GridLayout):
+    def convert(self):
+        try:
+            number = int(self.text_input.text)
+        except Exception:
+            number = 0
+
+        self.label_hours.text = str(number * 24)
+        self.label_minutes.text = str(number * 24 * 60)
+        self.label_seconds.text = str(number * 24 * 60 * 60)
+        self.label_milliseconds.text = str(number * 24 * 60 * 60 * 60)
+        try:
+            self.label_weeks.text = str("%.2f" % round(number / 7,2))
+        except Exception:
+            self.label_weeks.text = "Mistake"
 
 class DuckyApp(App):
     def build(self):
