6332efb8458f8d7a6584a0427cf050de44003bcf
tysunday <120739860+tysunday@users.noreply.github.com>
2023-10-20 13:40:52 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
-import sqlite3 as sql
+import pyttsx3
+import speech_recognition as sr
+import datetime
+import webbrowser
 
-# Подключение к базе данных.
-connection = sql.connect('test.sqlite')
-# Создание курсора для работы с БД
-q = connection.cursor()
+engine = pyttsx3.init()
 
-q.execute('''CREATE TABLE user (id int auto_increment primary key, name varchar, password varchar) ''')
-connection.commit()
+def sayToMe(talk):
+    engine.say(talk)
+    engine.runAndWait()
 
-name = input("Введите ваш имя: ")
-password = input("Введите пароль: ")
+sayToMe("Hello everyone, everything is worked")
+sayToMe("Say something")
+# for index, name in enumerate(sr.Microphone.list_microphone_names()):
+#      print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
 
-print
+record = sr.Recognizer()
+try:
+    with sr.Microphone(device_index=0) as source:
+        print("Say...")
+        audio = record.listen(source)
+        result = record.recognize_google(audio)
+        result = result.lower
+        print(result)
 
-# Отключение от базы данных
-q.close()
-connection.close()
+        if result == 'Say me time':
+            now = datetime.datetime.now()
+            str_date = "Now {}:{}".format(str(now.hour), str(now.minute))
+            print(str_date)
+            sayToMe(str_date)
+        elif result == "Open web-browser":
+            webbrowser.open("https://itproger.com")
+
+
+except sr.UnknownValueError:
+    print("The voice was not recognized")
+except sr.RequestError:
+    print("Something went wrong...")
+
+
